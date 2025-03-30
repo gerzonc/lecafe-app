e825ac2bfdab8f562976206d3585865e51c46ce9
gerzonc <gerzonc@icloud.com>
2025-03-30 12:54:01 -0400

    chore: remove ios folder from repository

--- constants/Colors.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  white: "#FFFFFF",
-  pastelPink: "#FFB1C7",
-  lightPink: "#FEB5DB",
-  hotPink: "#FF58A4",
-  dateGradient: ["#FF6B86", "#FFB03A"],
-  relationGradient: ["#FF58A4", "#FF6B86"],
-  friendshipGradient: ["#7086E3", "#9072E5"],
-};
