0946a1a0f311cc5918381a373fa15592cadc7076
gerzonc <gerzonc@icloud.com>
2025-03-30 10:30:53 -0400

    chore: remove default files

---
+++ app/index.tsx
@@ -0,0 +1,8 @@
+import { View } from "react-native";
+import { Text } from "react-native-gesture-handler";
+
+export default function App() {
+  return <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Hello World</Text>
+  </View>
+}
